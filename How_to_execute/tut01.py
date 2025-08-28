f5fd03795cf84fe678c301ae469f5ddd66eeb020
Anurag Chandel <76684963+AnuragDOES@users.noreply.github.com>
2025-08-29 00:13:44 +0530

    everything working

--- How_to_execute/tut01.py
+++ How_to_execute/tut01.py
@@ -11,7 +11,7 @@ def clear_folder(folder_path):
         os.remove(f)
 
 def branchwiseFullList(df):
-    output_folder = "../output/full_branchwise"
+    output_folder = "output/full_branchwise"
     os.makedirs(output_folder, exist_ok=True)
     for branch in unique_branches:
         branch_df = df[df["Branch"] == branch][header]
@@ -20,13 +20,13 @@ def branchwiseFullList(df):
 
 def branchMix(df):
     branches_data = {}
-    folder = "../output/full_branchwise"
+    folder = "output/full_branchwise"
     for filename in os.listdir(folder):
         branch_name = os.path.splitext(filename)[0]
         df = pd.read_csv(os.path.join(folder, filename))
         branches_data[branch_name] = df.values.tolist()
     groupNum = 0
-    output_folder = "../output/group_branch_wise_mix"
+    output_folder = "output/group_branch_wise_mix"
     os.makedirs(output_folder, exist_ok=True)
     while any(branches_data.values()):
         groupNum += 1
@@ -43,10 +43,10 @@ def branchMix(df):
         
         
 def uniformMix(df):
-    output_folder = "../output/group_uniform_mix"
+    output_folder = "output/group_uniform_mix"
     os.makedirs(output_folder, exist_ok=True)
     branches_data = {}
-    folder = "../output/full_branchwise"
+    folder = "output/full_branchwise"
     for filename in os.listdir(folder):
         branch_name = os.path.splitext(filename)[0]
         df = pd.read_csv(os.path.join(folder, filename))
@@ -96,7 +96,7 @@ n = st.number_input("Enter the number of groups:", min_value=1, step=1)
 inputFile = st.file_uploader("Input excel file", type=["xlsx"])
 
 if inputFile is not None:
-    input_path = os.path.join("../input",inputFile.name)
+    input_path = os.path.join("input",inputFile.name)
     with open(input_path,"wb") as f:
         f.write(inputFile.getbuffer())
     st.success(f"{inputFile.name} saved to input folder")
@@ -113,20 +113,20 @@ if inputFile is not None:
     
 if st.button("Start grouping"):
     if inputFile is not None and n>0:
-        clear_folder("../output/full_branchwise")
-        clear_folder("../output/group_branch_wise_mix")
-        clear_folder("../output/group_uniform_mix")
+        clear_folder("output/full_branchwise")
+        clear_folder("output/group_branch_wise_mix")
+        clear_folder("output/group_uniform_mix")
         branchwiseFullList(df)
         branchMix(df)
         uniformMix(df)
         #output file code
-        filepath = "../output.xlsx"
+        filepath = "output.xlsx"
         if not os.path.exists(filepath):
             empty_df = pd.DataFrame()
             empty_df.to_excel(filepath, index=False)
-        df1 = makeOutputFile("../output/group_branch_wise_mix",df)
-        df2 = makeOutputFile("../output/group_uniform_mix",df)
-        with pd.ExcelWriter("../output/output.xlsx") as writer:
+        df1 = makeOutputFile("output/group_branch_wise_mix",df)
+        df2 = makeOutputFile("output/group_uniform_mix",df)
+        with pd.ExcelWriter("output/output.xlsx") as writer:
             df1.to_excel(writer, sheet_name="Stats", index=False)
             start_row_df2 = len(df1) + 2 
             df2.to_excel(writer, sheet_name="Stats", startrow=start_row_df2, index=False)
